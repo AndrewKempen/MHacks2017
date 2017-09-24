0c1eb20285d0955c7d6e23c248c1d408a77dfd18
Wishbonea <akemperfi@gmail.com>
2017-09-24 02:37:32 -0400

    Very close to working

--- app/src/main/java/com/github/wishbonea/decode/TranslateActivity.java
+++ app/src/main/java/com/github/wishbonea/decode/TranslateActivity.java
@@ -1,8 +1,11 @@
 package com.github.wishbonea.decode;
 
 import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
@@ -12,14 +15,195 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.github.akemperfi.decode.R;
+import com.microsoft.bing.speech.SpeechClientStatus;
+import com.microsoft.cognitiveservices.speechrecognition.DataRecognitionClient;
+import com.microsoft.cognitiveservices.speechrecognition.ISpeechRecognitionServerEvents;
+import com.microsoft.cognitiveservices.speechrecognition.MicrophoneRecognitionClient;
+import com.microsoft.cognitiveservices.speechrecognition.RecognitionResult;
+import com.microsoft.cognitiveservices.speechrecognition.RecognitionStatus;
+import com.microsoft.cognitiveservices.speechrecognition.SpeechRecognitionMode;
+import com.microsoft.cognitiveservices.speechrecognition.SpeechRecognitionServiceFactory;
+import java.io.InputStream;
 
 import java.util.ArrayList;
 
-public class TranslateActivity extends AppCompatActivity implements View.OnClickListener {
+public class TranslateActivity extends AppCompatActivity implements ISpeechRecognitionServerEvents {
+    int m_waitSeconds = 0;
+    DataRecognitionClient dataClient = null;
+    MicrophoneRecognitionClient micClient = null;
+    FinalResponseStatus isReceivedResponse = FinalResponseStatus.NotReceived;
+
+    public enum FinalResponseStatus { NotReceived, OK, Timeout }
+
+    /**
+     * Gets the default locale.
+     * @return The default locale.
+     */
+    private String getDefaultLocale() {
+        return "en-us";
+    }
+
+    private void createListener(View view) {
+        System.out.println("Button clicked");
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            System.out.println("NOT GIVEN RECORDING PERMISSIONS!");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 0);
+        }
+        this.m_waitSeconds = 200;
+        if (this.micClient == null) {
+            this.micClient = SpeechRecognitionServiceFactory.createMicrophoneClient(this,
+                    SpeechRecognitionMode.LongDictation, "en-US", this,
+                    "7ce06d0e34b94ee4b96400c7a69d754b", "4d07a480f75441c6b2c2c82a0245f950");
+        }
+        this.micClient.startMicAndRecognition();
+        System.out.println("Completed!");
+    }
+
+    private View.OnClickListener recordButtonListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            createListener(v);
+        }
+    };
+
+    public void onFinalResponseReceived(final RecognitionResult response) {
+        boolean isFinalDicationMessage = (response.RecognitionStatus == RecognitionStatus.EndOfDictation ||
+                response.RecognitionStatus == RecognitionStatus.DictationEndSilenceTimeout);
+        if (null != this.micClient && isFinalDicationMessage) {
+            // we got the final result, so it we can end the mic reco.  No need to do this
+            // for dataReco, since we already called endAudio() on it as soon as we were done
+            // sending all the data.
+            this.micClient.endMicAndRecognition();
+        }
+
+        if (isFinalDicationMessage) {
+            this.isReceivedResponse = FinalResponseStatus.OK;
+        }
+
+        if (!isFinalDicationMessage) {
+            this.WriteLine("********* Final n-BEST Results *********");
+            for (int i = 0; i < response.Results.length; i++) {
+                this.WriteLine("[" + i + "]" + " Confidence=" + response.Results[i].Confidence +
+                        " Text=\"" + response.Results[i].DisplayText + "\"");
+            }
+            this.WriteLine();
+        }
+    }
+
+    /**
+     * Called when a final response is received and its intent is parsed
+     */
+    public void onIntentReceived(final String payload) {
+        this.WriteLine("--- Intent received by onIntentReceived() ---");
+        this.WriteLine(payload);
+        this.WriteLine();
+    }
+
+    public void onPartialResponseReceived(final String response) {
+        this.WriteLine("--- Partial result received by onPartialResponseReceived() ---");
+        this.WriteLine(response);
+        this.WriteLine();
+    }
+
+    public void onError(final int errorCode, final String response) {
+        this.WriteLine("--- Error received by onError() ---");
+        this.WriteLine("Error code: " + SpeechClientStatus.fromInt(errorCode) + " " + errorCode);
+        this.WriteLine("Error text: " + response);
+        this.WriteLine();
+    }
+
+    /**
+     * Called when the microphone status has changed.
+     * @param recording The current recording state
+     */
+    public void onAudioEvent(boolean recording) {
+        this.WriteLine("--- Microphone status change received by onAudioEvent() ---");
+        this.WriteLine("********* Microphone status: " + recording + " *********");
+        if (recording) {
+            this.WriteLine("Please start speaking.");
+        }
+
+        WriteLine();
+        if (!recording) {
+            this.micClient.endMicAndRecognition();
+        }
+    }
+
+    /**
+     * Writes the line.
+     */
+    private void WriteLine() {
+        this.WriteLine("");
+    }
+
+    /**
+     * Writes the line.
+     * @param text The line to write.
+     */
+    private void WriteLine(String text) {
+        System.out.println(text);
+    }
+
+    /*
+     * Speech recognition with data (for example from a file or audio source).
+     * The data is broken up into buffers and each buffer is sent to the Speech Recognition Service.
+     * No modification is done to the buffers, so the user can apply their
+     * own VAD (Voice Activation Detection) or Silence Detection
+     *
+     * @param dataClient
+     * @param recoMode
+     * @param filename
+     */
+    private class RecognitionTask extends AsyncTask<Void, Void, Void> {
+        DataRecognitionClient dataClient;
+        SpeechRecognitionMode recoMode;
+        String filename;
+
+        RecognitionTask(DataRecognitionClient dataClient, SpeechRecognitionMode recoMode, String filename) {
+            this.dataClient = dataClient;
+            this.recoMode = recoMode;
+            this.filename = filename;
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                // Note for wave files, we can just send data from the file right to the server.
+                // In the case you are not an audio file in wave format, and instead you have just
+                // raw data (for example audio coming over bluetooth), then before sending up any
+                // audio data, you must first send up an SpeechAudioFormat descriptor to describe
+                // the layout and format of your raw audio data via DataRecognitionClient's sendAudioFormat() method.
+                // String filename = recoMode == SpeechRecognitionMode.ShortPhrase ? "whatstheweatherlike.wav" : "batman.wav";
+                InputStream fileStream = getAssets().open(filename);
+                int bytesRead = 0;
+                byte[] buffer = new byte[1024];
+
+                do {
+                    // Get  Audio data to send into byte buffer.
+                    bytesRead = fileStream.read(buffer);
+
+                    if (bytesRead > -1) {
+                        // Send of audio data to service.
+                        dataClient.sendAudio(buffer, bytesRead);
+                    }
+                } while (bytesRead > 0);
+
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+            finally {
+                dataClient.endAudio();
+            }
+
+            return null;
+        }
+    }
     private SpeechRecognizer sr;
     private static final String TAG = "Translation";
     private String sourceLanguageMessage;
@@ -36,7 +220,7 @@ public class TranslateActivity extends AppCompatActivity implements View.OnClick
         sourceLanguageMessage = intent.getStringExtra(MainActivity.SOURCE_LANGUAGE_MESSAGE);
         targetLanguageMessage = intent.getStringExtra(MainActivity.TARGET_LANGUAGE_MESSAGE);
         ImageButton speakButton = (ImageButton) findViewById(R.id.recordButton);
-        speakButton.setOnClickListener(this);
+        speakButton.setOnClickListener(recordButtonListener);
         sr = SpeechRecognizer.createSpeechRecognizer(this);
         sr.setRecognitionListener(new listener());
         sourceLanguageText = (TextView)findViewById(R.id.sourceLanguageText);
@@ -96,39 +280,6 @@ public class TranslateActivity extends AppCompatActivity implements View.OnClick
     }
 
     public void onClick(View v) {
-        System.out.println("Button clicked");
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            System.out.println("NOT GIVEN RECORDING PERMISSIONS!");
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 0);
-        }
-        if (v.getId() == R.id.recordButton) {
-            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-            intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, "voice.recognition.test");
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, sourceLanguageMessage);
-
-            intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
-            sr.startListening(intent);
-            sourceLanguageText.setTextColor(ContextCompat.getColor(this, R.color.black));
-            sourceLanguageText.setText("Listening...");
-        }
-
-
-        String inputText = "<Blabla>What we want</Blabla>";
-        String outputText = "";
-        for(int i = 0; i < inputText.length(); i++) {
-            if(inputText.charAt(i) == '>') {
-                i++;
-                for(;i < inputText.length(); i++) {
-                    if(inputText.charAt(i) != '<') {
-                        outputText += inputText.charAt(i);
-                    } else {
-                        break;
-                    }
-                }
-                break;
-            }
-        }
 
     }
 }
